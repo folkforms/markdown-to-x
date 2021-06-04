946da71159fbf501d97c696b6f83ba5ba456231f
folkforms <alan.bergin@gmail.com>
2021-06-04 23:33:33 +0100

    Working version of templates.js (part 2)

---
+++ packages/templates/src/replaceParam.js
@@ -0,0 +1,50 @@
+const replaceParam = (line, paramData, data) => {
+  // Define search param
+  const searchParam = `%${paramData.param}${paramData.qualifier || ""}%`;
+
+  // Calculate indentation
+  let indentation = "";
+  const leftPart = line.substring(0, line.indexOf(searchParam));
+  if(!leftPart.match(/\S+/)) {
+    indentation = leftPart;
+  }
+
+  // Create replacement data
+  let replacement;
+  if(paramData.qualifier) {
+    let found;
+    const qualifier = paramData.qualifier.substring(1, paramData.qualifier.length - 1); // Remove brackets
+    if(found = qualifier.match("line:(\\d)")) {
+      let line = found[1];
+      replacement = ensureArray(data[paramData.param])[line];
+    } else if(found = qualifier.match("indent:(\\d)")) {
+      let indent = found[1];
+      replacement = ensureArray(data[paramData.param]);
+      replacement = applyIndentation(replacement, indent);
+    } else {
+      throw new Error(`Unknown qualifier: '${paramData.qualifier}' => '${qualifier}'`);
+    }
+  } else {
+    replacement = ensureArray(data[paramData.param]);
+  }
+  
+  // Replace the parameter
+  replacement = ensureArray(replacement);
+  replacement = replacement.join("\n");
+  const x = line.replace(`${searchParam}`, replacement);
+  return ensureArray(x.split("\n"));
+}
+
+const ensureArray = item => {
+  return item = typeof item === "string" ? [ item ] : item;
+}
+
+const applyIndentation = (arr, indent) => {
+  let prefix = "";
+  while(prefix.length < indent) {
+    prefix += " ";
+  }
+  return arr.map(item => prefix + item);
+}
+
+module.exports = { replaceParam };
