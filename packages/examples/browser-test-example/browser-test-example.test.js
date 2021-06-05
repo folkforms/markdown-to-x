f3a61064e493385558e91ac33cbfde2236b9bdb1
folkforms <alan.bergin@gmail.com>
2021-06-05 19:02:55 +0100

    Example test now tests both output files

--- packages/examples/browser-test-example/browser-test-example.test.js
+++ packages/examples/browser-test-example/browser-test-example.test.js
@@ -9,13 +9,16 @@ test('browser test example 1', () => {
   const mappingsData = fileio.readJson("browser-test-example/inputs/bte-mappings.json");
   const templateFilename = "browser-test-example/inputs/bte-template.js";
   const templateData = fileio.readLines(templateFilename);
-  const expected = fileio.readLines("browser-test-example/expected/bte-doc1.js");
   const outputFolder = "browser-test-example/outputs";
+  const expected1 = fileio.readLines("browser-test-example/expected/bte-doc1.js");
+  const expected2 = fileio.readLines("browser-test-example/expected/bte-doc2.js");
 
   // Act
   main(inputFiles, structure, mappingsData, templateData, templateFilename, );
-  const actual = fileio.readLines(`${outputFolder}/bte-doc1.js`);
+  const actual1 = fileio.readLines(`${outputFolder}/bte-doc1.js`);
+  const actual1 = fileio.readLines(`${outputFolder}/bte-doc2.js`);
 
   // Assert
-  expect(actual).toEqual(expected);
+  expect(actual1).toEqual(expected1);
+  expect(actual2).toEqual(expected2);
 });
