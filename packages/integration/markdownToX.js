f8839e994954c216b3fb8d317677dd579ac21c36
folkforms <alan.bergin@gmail.com>
2021-11-06 10:44:12 +0000

    Add jsdoc to main function

--- packages/integration/markdownToX.js
+++ packages/integration/markdownToX.js
@@ -2,10 +2,23 @@ const markdownToJs = require("../markdown-to-js/markdown-to-js");
 const mappings = require("../mappings/mappings");
 const templates = require("../templates/templates");
 
-const markdownToX = (input, structure, mappingsData, templateData, filename) => {
+/**
+ * Converts the input file to the output format using the given structure, template and (optionally)
+ * mappings files.
+ *
+ * Note that the output is not necessarily JSON. It is in whatever format the template contained.
+ *
+ * @param {array} input input data array
+ * @param {array} structure structure file array
+ * @param {object} mappingsObj mappings data object (optional)
+ * @param {array} templateData template data array
+ * @param {string} filename input filename
+ * @return an array of strings containing the output data
+ */
+const markdownToX = (input, structure, mappingsObj, templateData, filename) => {
   const additionalData = { "@filename": filename };
   const obj = markdownToJs.execute(structure, input);
-  const mapped = mappingsData ? mappings.execute(obj, mappingsData) : obj;
+  const mapped = mappingsObj ? mappings.execute(obj, mappingsObj) : obj;
   const templated = templates.execute(templateData, mapped, additionalData);
   return templated;
 }
